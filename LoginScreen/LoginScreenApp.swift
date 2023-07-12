82babf9f4ec8193f6e5d1a05d0e3216b3be4bae3
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-07-12 14:11:34 +0200

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Piotr Woźniak on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
